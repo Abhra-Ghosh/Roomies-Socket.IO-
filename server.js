cc8e1a2fcb6737447e9817dda6722657127477f5
Abhra-Ghosh <abhraghosh9@gmail.com>
2020-10-09 23:12:34 +0530

    roomies__webRTC

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  //console.log("");
+const PORT =process.env.PORT || 3000
+app.listen(PORT, () => {
   console.log("server running on port 3000");
 });
